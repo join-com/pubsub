05e9d82a1cca3524f38193a21668c9eb69cf16be
Evgeniy Kirpichenko <evkirpichenko@gmail.com>
2022-06-21 16:27:03 +0200

    fix: TECH accept logger as PublisherFactory constructor attr

--- packages/pubsub/src/PublisherFactory.ts
+++ packages/pubsub/src/PublisherFactory.ts
@@ -1,4 +1,5 @@
 import { PubSub } from '@google-cloud/pubsub'
+import { ILogger } from './ILogger'
 import { Publisher } from './Publisher'
 
 export interface IPublisher<T> {
@@ -10,11 +11,11 @@ export interface IPublisher<T> {
 export class PublisherFactory<T> {
   private readonly client: PubSub
 
-  constructor() {
+  constructor(private readonly logger?: ILogger) {
     this.client = new PubSub()
   }
 
   public getPublisher<K extends keyof T>(topic: K): IPublisher<T[K]> {
-    return new Publisher(topic.toString(), this.client)
+    return new Publisher(topic.toString(), this.client, this.logger)
   }
 }

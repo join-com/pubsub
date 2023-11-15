a1f908f8be60718b894f3fda798896c7d884cf08
Ievgen Kyrpychenko <evkirpichenko@gmail.com>
2023-11-15 17:39:33 +0100

    fix: TECH set jest worker memory limit

--- packages/pubsub/jest.config.js
+++ packages/pubsub/jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   setupFilesAfterEnv: ['jest-extended/all'],
   testPathIgnorePatterns: ['/node_modules/', 'generated', 'support'],
   watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+  workerIdleMemoryLimit: '1.5GB',
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',

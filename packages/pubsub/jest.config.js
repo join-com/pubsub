53abaf8c0445cb8d34a1cd801b4ad3e4bc57e7b4
Ievgen Kyrpychenko <evkirpichenko@gmail.com>
2023-11-15 17:51:03 +0100

    fix: TECH switch to swc

--- packages/pubsub/jest.config.js
+++ packages/pubsub/jest.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-  preset: 'ts-jest',
+  runtime: '@side/jest-runtime',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['jest-extended/all'],
   testPathIgnorePatterns: ['/node_modules/', 'generated', 'support'],
   watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
   workerIdleMemoryLimit: '1.5GB',
   transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        diagnostics: false,
-      },
-    ],
+    '^.+\\.tsx?$': '@swc/jest',
   },
 }

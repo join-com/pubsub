c38588c76043966f67e0812ac31b6ba4cf174e8c
Eugene Taran <eugene.taran@join.com>
2025-02-10 10:54:06 +0100

    fix: JOIN-50367 add default ttl

--- packages/pubsub/src/RedisIdempotencyStorage.ts
+++ packages/pubsub/src/RedisIdempotencyStorage.ts
@@ -7,8 +7,13 @@ export interface IIdempotencyStorage {
 }
 
 export class RedisIdempotencyStorage implements IIdempotencyStorage {
+  /**
+   * Constructor
+   * @param redisClient implementation of IRedisClient
+   * @param redisDefaultSecondsTtl redis default Time-to-Live for the message key, by default set to 2 hours
+   */
   constructor(private readonly redisClient: IRedisClient,
-              private readonly redisDefaultTtl: number) {}
+              private readonly redisDefaultSecondsTtl: number = 7200) {}
 
   public async exists(key: string): Promise<boolean> {
     const value = await this.redisClient.get(key)
@@ -21,6 +26,6 @@ export class RedisIdempotencyStorage implements IIdempotencyStorage {
 
   public async save(key: string): Promise<void> {
     const value = Buffer.from(new Date().toISOString())
-    await this.redisClient.setex(key, this.redisDefaultTtl, value)
+    await this.redisClient.setex(key, this.redisDefaultSecondsTtl, value)
   }
 }
